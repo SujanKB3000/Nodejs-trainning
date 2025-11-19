16a3188ef3360fb83267bbcfa88711dc3cc09064
sujan <sujnet666@gmail.com>
2025-11-19 07:00:18 +0545

    Test01

--- index.js
+++ index.js
@@ -3,5 +3,12 @@
 // let program="Hello";
 // console.log(program);
 
-const pi=3.14;
-console.log(pi);
+// const pi=3.14;
+// console.log(pi);
+
+let x=10;
+if(true){
+    let x=20;
+    console.log("Inside",x);
+}
+console.log("Outside",x);
